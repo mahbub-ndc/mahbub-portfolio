6537490dba1bc55552da16d29e4f4d3439fb995a
mahbub-ndc <mahbub5015@gmail.com>
2024-09-23 00:52:13 +0600

    project setup [done]

---
+++ src/components/Banner.tsx
@@ -0,0 +1,64 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import { TypeAnimation } from "react-type-animation";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import headerImage from "../images/night-long.jpg";
+
+const Banner = () => {
+  return (
+    <section className="lg:py-6">
+      <div className="grid grid-cols-1 sm:grid-cols-12">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+        >
+          <h1 className="text-white mb-4 text-xl sm:text-2xl lg:text-5xl lg:leading-normal font-extrabold">
+            <span className="gradinent-text">Hello, I&apos;m Mahbub</span>
+            <br></br>
+            <TypeAnimation
+              sequence={[
+                "Full Stack Web Developer",
+                1000,
+                "Web Application Developer",
+                1000,
+              ]}
+              wrapper="span"
+              speed={50}
+              repeat={Infinity}
+            />
+          </h1>
+
+          <div>
+            <Link href="/#contact">
+              <button className="btn btn-outline btn-secondary">
+                Download Resume{" "}
+              </button>
+            </Link>
+          </div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="col-span-4 place-self-center mt-4 lg:mt-0"
+        >
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+            <Image
+              src={headerImage}
+              alt="hero image"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              width={300}
+              height={300}
+            />
+          </div>
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
